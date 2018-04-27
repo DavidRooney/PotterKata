930da754d6bb147da0cf589a573fdd55be4f70e8
Rooney, David <david_rooney16@hotmail.com>
2018-04-27 08:43:37 +0100

    commit to not lose progress - ignore htis one

--- PotterKata/Service/PriceCalculatorService.cs
+++ PotterKata/Service/PriceCalculatorService.cs
@@ -1,15 +1,28 @@
 ﻿using PotterKata.Entities;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace PotterKata.Service
 {
-    public class PriceCalculatorService
+    public static class PriceCalculatorService
     {
+        private static readonly Dictionary<int, int> Discounts = new Dictionary<int, int>() { { 1, 0 }, { 2, 5 }, { 3, 10 } };
+
         public static double CalculateTotalPrice(Book[] books)
         {
             var totalCost = books.Sum(b => b.Cost);
             var basketCount = books.Count();
             var uniqueBooksInBasket = books.Distinct();
+
+            var discount = Discounts[uniqueBooksInBasket.Count()];
+
+            var groupedBooks = books.GroupBy(book => book.Name);
+
+            for (int i = 0; i < groupedBooks.Count(); i++)
+            {
+
+            }
+
 
             if (basketCount == 2 && uniqueBooksInBasket.Count() == basketCount)
             {
